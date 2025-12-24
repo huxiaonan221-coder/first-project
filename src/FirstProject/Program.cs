6fb699315983b1850d54b614d960ec0c6fdcc940
huxiaonan221-coder <huxiaonan221@gmail.com>
2025-12-24 15:57:07 +0800

    Add .NET project scaffold

---
+++ src/FirstProject/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstProject;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello from FirstProject!");
+    }
+}
